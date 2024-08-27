56796dc1abeda9b472482b34ff69fd01e5ef1114
aallm004 <abigailrosea10@gmail.com>
2024-08-27 18:42:21 -0500

    documentation correction

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,19 +3,6 @@ def matrix_shape(matrix):
     """
     Function to calculate the shape of a matrix.
     """
-    # Initialize an empty list to store the shape
-    shape = []
-
-    # Iterate until the matrix is not a list (i.e., a scalar value)
-    while isinstance(matrix, list):
-        # Append the length of the current list to the shape
-        shape.append(len(matrix))
-
-        # Move to the next nested list
-        matrix = matrix[0]
-
-    # Return the shape
-    return shape"""
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
