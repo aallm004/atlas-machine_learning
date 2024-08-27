cf83074e8c8fb5419f51b983c4856ed385fc232c
aallm004 <abigailrosea10@gmail.com>
2024-08-27 18:40:17 -0500

    corrected tab settings in vscode

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
-  if not matrix:
-    return []
-  
-  shape = []
-
-  while isinstance(matrix, list):
-    shape.append(len(matrix))
-    matrix = matrix[0]
-  return shape
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
