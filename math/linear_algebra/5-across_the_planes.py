ee64b198a9d20e83ff8fb806fe7f38022428e85f
aallm004 <abigailrosea10@gmail.com>
2024-08-28 15:19:55 -0500

    task 4 && 5

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,15 @@
+#1/usr/bin/env python3
+"""function that adds two martrices elemt-wise"""
+
+
+def add_matrices2D(mat1, mat2):
+    """adds two martrices elemt-wise"""
+    result = []
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat1[i])):
+            row.append(mat1[i][j] + mat2[i][j])
+        result.append(row)
+    return result
