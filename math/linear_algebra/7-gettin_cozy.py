b31fb689e06b593997ad479f207393d9c4a74bc9
aallm004 <abigailrosea10@gmail.com>
2024-08-28 16:15:43 -0500

    added code for unmatching dimensions and a 2d empty matrix

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -5,8 +5,12 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     """concatenates two matrices along a specific axis"""
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         return mat1 + mat2
     elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
         return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
     else:
         return None
