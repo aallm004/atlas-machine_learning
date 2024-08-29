352781ab5587b6540596fc19b3cff155f62cbda3
aallm004 <abigailrosea10@gmail.com>
2024-08-29 11:37:46 -0500

    corrected number of lines in task 9

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
-"""Complete the following source code (found below):
-
-mat1 should be the middle two rows of matrix
-mat2 should be the middle two columns of matrix
-mat3 should be the bottom-right, square, 3x3 matrix of matrix
-You are not allowed to use any loops or conditional statements
-Your program should be exactly 10 lines"""
 import numpy as np
-
 matrix = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12],
                    [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24]])
 mat1 = matrix[1:3]
