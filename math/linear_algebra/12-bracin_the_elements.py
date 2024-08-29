2a7d980e49d3138cd57091a063706515450db243
aallm004 <abigailrosea10@gmail.com>
2024-08-29 12:00:37 -0500

    task 12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python3
-def matrix_shape(matrix):
-  if not matrix:
-    return []
-  
-  shape = []
-  current_level = matrix
+"""Function that performs element-wise add, sub, mult and div"""
 
-  while isinstance(current_level, list):
-    shape.append(len(current_level))
-    first_row_length = len(current_level[0]) if current_level else 0
-    for row in current_level:
-      if len(row) != first_row_length:
-        raise ValueError("Matrix is not rectangular")
-      current_level = current_level[0]
 
-  return shape
+def np_elementwise(mat1, mat2):
+    """Function that performs element-wise add, sub, mult and div"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
