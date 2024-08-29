4998af90b90a335726db3a74e02958f44c76ea48
aallm004 <abigailrosea10@gmail.com>
2024-08-29 11:12:24 -0500

    task 8

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-import numpy as np
-
+"""Function that performs matrix multiplication"""
 
 def mat_mul(mat1, mat2):
-    res = np.dot(mat1, mat2)
-
-    print(res)
+    if len(mat1[0]) != len(mat2):
+        return None
+    mul = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                mul[i][j] += mat1[i][k] * mat2[k][j]
+    return mul
