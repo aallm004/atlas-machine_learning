5321c6bd4e0f48a2caaa49b7c0f38b08c05052b3
aallm004 <abigailrosea10@gmail.com>
2024-08-29 13:35:04 -0500

    pycodestyle task 14

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -2,6 +2,7 @@
 """function that performs matrix multiplication"""
 import numpy as np
 
+
 def np_matmul(mat1, mat2):
     """function that performs matrix multiplication"""
     return np.matmul(mat1, mat2)
