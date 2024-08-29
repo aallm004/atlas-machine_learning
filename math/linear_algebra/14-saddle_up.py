c4a817f7175ab992cf412ca7c9f00be7099c777b
aallm004 <abigailrosea10@gmail.com>
2024-08-29 13:32:38 -0500

    tasks 13&&14

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python3
-def matrix_shape(matrix):
-  if not matrix:
-    return []
-  
-  shape = []
-  current_level = matrix
+"""function that performs matrix multiplication"""
+import numpy as np
 
-  while isinstance(current_level, list):
-    shape.append(len(current_level))
-    first_row_length = len(current_level[0]) if current_level else 0
-    for row in current_level:
-      if len(row) != first_row_length:
-        raise ValueError("Matrix is not rectangular")
-      current_level = current_level[0]
-
-  return shape
+def np_matmul(mat1, mat2):
+    """function that performs matrix multiplication"""
+    return np.matmul(mat1, mat2)
