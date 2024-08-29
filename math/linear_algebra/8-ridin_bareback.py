5c4575a2c356152a23821746ca02a879d9c681f3
aallm004 <abigailrosea10@gmail.com>
2024-08-29 11:13:53 -0500

    pycodestyle

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Function that performs matrix multiplication"""
 
+
 def mat_mul(mat1, mat2):
     """Function that performs matrix multiplication"""
     if len(mat1[0]) != len(mat2):
