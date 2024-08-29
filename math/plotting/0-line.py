54552ef29ca6033991c6b870b2ca459b96d887ec
aallm004 <abigailrosea10@gmail.com>
2024-08-29 14:13:39 -0500

    created files and completed task 0-line_plot.png

---
+++ math/plotting/0-line.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line():
+
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(np.arange(0,11), y, 'r-')
+
+    plt.xlim(0,10)
+
+    plt.xlabel('x')
+    plt.xlabel('y')
+    plt.title('Cubic Function')
+
+    plt.savefig('line_plot.png')
+    plt.close()
