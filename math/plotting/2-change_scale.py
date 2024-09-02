50037072f4a0dbd121853af7d3467d3ad5bd38c1
aallm004 <abigailrosea10@gmail.com>
2024-09-02 14:50:20 -0500

    task 2

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""line graph"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def change_scale():
+    """line graph"""
     x = np.arange(0, 28651, 5730)
     r = np.log(0.5)
     t = 5730
