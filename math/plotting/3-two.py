4e4031b563b6d49d62f92d3af3a53b5712da4e64
aallm004 <abigailrosea10@gmail.com>
2024-09-02 15:01:52 -0500

    task 3

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""plotting line graph"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def two():
+    """plotting line graph"""
 
     x = np.arange(0, 21000, 1000)
     r = np.log(0.5)
@@ -12,4 +15,14 @@ def two():
     y2 = np.exp((r / t2) * x)
     plt.figure(figsize=(6.4, 4.8))
 
-    # your code here
+
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of Radioactive Elements')
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1.1)
+    plt.legend(loc='upper right')
+    plt.plot(x, y1, 'r--', label='C-14')
+    plt.plot(x, y2, 'g-', label='Ra-226')
+
+    plt.show()
