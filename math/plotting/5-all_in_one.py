87c0855c7021e2b1007ebc5d7c3d054794023b54
aallm004 <abigailrosea10@gmail.com>
2024-09-02 16:07:24 -0500

    changed x and y ticks for 5

--- math/plotting/5-all_in_one.py
+++ math/plotting/5-all_in_one.py
@@ -33,12 +33,19 @@ def all_in_one():
     # First plot - line
     axs[0, 0].plot(np.arange(0, 11), y0, 'r-')
     axs[0, 0].set_xlim(0, 10)
-    axs[0, 0].set_ylabel('y', fontsize='x-small')
-    axs[0, 0].set_xlabel('x', fontsize='x-small')
-    axs[0, 0].set_title('y = x^3', fontsize='x-small')
+    axs[0, 0].set_ylim(0, 1000)
+    axs[0, 0].set_xticks([0, 2, 4, 6, 8, 10])
+    axs[0, 0].set_yticks([0, 500, 1000])
+    axs[0, 0].set_ylabel('', fontsize='x-small')
+    axs[0, 0].set_xlabel('', fontsize='x-small')
+    axs[0, 0].set_title('', fontsize='x-small')
 
     # Second plot - scatter
     axs[0, 1].scatter(x1, y1, c='m', s=10)
+    axs[0, 1].set_xlim(55, 85)
+    axs[0, 1].set_ylim(165, 195)
+    axs[0, 1].set_xticks([60, 70, 80])
+    axs[0, 1].set_yticks([170, 180, 190])
     axs[0, 1].set_xlabel('Height (in)', fontsize='x-small')
     axs[0, 1].set_ylabel('Weight (lbs)', fontsize='x-small')
     axs[0, 1].set_title("Men's Height vs Weight", fontsize='x-small')
@@ -50,6 +57,9 @@ def all_in_one():
     axs[1, 0].set_title('Exponential Decay of C-14', fontsize='x-small')
     axs[1, 0].set_yscale('log')
     axs[1, 0].set_xlim(0, 28650)
+    axs[1, 0].set_ylim(10**-1, 10**0)
+    axs[1, 0].set_xticks([0, 10000, 20000])
+    axs[1, 0].set_yticks([1, 1e-1])
 
     # Fourth plot - two line graphs
     axs[1, 1].plot(x3, y31, 'r--', label='C-14')
@@ -60,6 +70,8 @@ def all_in_one():
     axs[1, 1].legend(loc='upper right', fontsize='x-small')
     axs[1, 1].set_xlim(0, 20000)
     axs[1, 1].set_ylim(0, 1)
+    axs[1, 1].set_xticks([0, 5000, 10000, 15000, 20000])
+    axs[1, 1].set_yticks([0, 0.5, 1])
 
     # Fifth plot - Histogram
     axs[2, 0] = plt.subplot2grid((3, 2), (2, 0), colspan=2)
@@ -69,6 +81,8 @@ def all_in_one():
     axs[2, 0].set_title('Project A', fontsize='x-small')
     axs[2, 0].set_xlim(0, 100)
     axs[2, 0].set_ylim(0, 30)
+    axs[2, 0].set_xticks([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+    axs[2, 0].set_yticks([0, 10, 20, 30])
 
     plt.tight_layout()
     plt.show()
