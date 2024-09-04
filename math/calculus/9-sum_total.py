cb10dbfc4f3bba47041dee34909f9e9cb2706476
aallm004 <abigailrosea10@gmail.com>
2024-09-04 16:55:33 -0500

    task 9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Defines summation_i_squared"""
+
+
+def summation_i_squared(n):
+    """calculates sum of squares of numbers"""
+    if not isinstance(n, int) or n < 1:
+        return None
+    
+    result = (n * (n + 1) * (2 * n + 1)) // 6
+
+    return result
