fce5b3030539c3065fa80926d16a5dd1c943348d
aallm004 <abigailrosea10@gmail.com>
2024-09-04 16:56:25 -0500

    pycodestyle fix -- whitespace line 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,7 +6,7 @@ def summation_i_squared(n):
     """calculates sum of squares of numbers"""
     if not isinstance(n, int) or n < 1:
         return None
-    
+
     result = (n * (n + 1) * (2 * n + 1)) // 6
 
     return result
