d7db7c7dbd344dc903f58f2c34d758158bcc054c
aallm004 <abigailrosea10@gmail.com>
2024-09-06 14:10:22 -0500

    task 1

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -19,3 +19,18 @@ class Poisson:
                 raise ValueError("data must contain multiple values")
             else:
                 self.lambtha = (sum(data)) / len(data)
+
+    def pmf(self, k):
+        """Calculates the value of the PMF for a given number of successes"""
+        k = int(k)
+
+        if k < 0:
+            return 0
+        else:
+            factorial = 1
+            for i in range(1, k + 1):
+                factorial *= i
+            pmf = ((self.lambtha ** k) * (2.7182818285 ** -self.lambtha)
+                   ) / factorial
+
+        return pmf
