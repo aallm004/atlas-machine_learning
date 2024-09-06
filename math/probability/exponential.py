07ab3f50d464a0ffd64f4447c341a5673adc6ae5
aallm004 <abigailrosea10@gmail.com>
2024-09-06 14:28:47 -0500

    task 3

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+class Exponential:
+    """Exponential class"""
+    def __init__(self, data=None, lambtha=1.):
+        """Exponential constructor"""
+        self.lambtha = float(lambtha)
+
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.lambtha = (sum(data)) / len(data) 
+
