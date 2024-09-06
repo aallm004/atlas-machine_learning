997497d7f1020ec97544475b9c1dafa327a99fc2
aallm004 <abigailrosea10@gmail.com>
2024-09-06 13:50:49 -0500

    pushing to see what checker says to me

---
+++ math/probability/poisson.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""poisson distrubution"""
+
+
+class Poisson:
+    """Poisson class"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """Poisson constructor"""
+        self.lambtha = float(lambtha)
+
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                if not isinstance(data, list):
+                    raise TypeError("data must be a list")
+                elif len(data) < 2:
+                    raise ValueError("data must contain multiple values")
+                else:
+                    self.lambtha = float(sum(data)) / len(data)
