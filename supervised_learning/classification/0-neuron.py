79c038fe572a089a8db1f681e87e720ac2491546
aallm004 <abigailrosea10@gmail.com>
2024-09-16 13:48:00 -0500

    task 0

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+class Neuron:
+    """A class that defines a single neuron performing binary classification"""
+    def __init__(self, nx):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
