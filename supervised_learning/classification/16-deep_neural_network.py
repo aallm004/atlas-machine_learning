f172aed928023e56d36fbb5403b859b442f33caa
aallm004 <abigailrosea10@gmail.com>
2024-09-18 18:01:20 -0500

    ah

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -17,14 +17,10 @@ class DeepNeuralNetwork:
 
         self.L = len(layers)
 
-        layers.insert(0, nx)
-        
         self.cache = {}
         self.weights = {}
         
         for l in range(1, self.L + 1):
-            if layers[l] < 1:
-                raise TypeError("layers must be a list of positive integers")
             layer_size = layers[l - 1]
             prev_layer_size = nx if l == 1 else layers[l - 2]
 
