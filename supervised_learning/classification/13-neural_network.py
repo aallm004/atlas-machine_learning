3fc271bd552ec402fdf3edcded321c94396c09b1
aallm004 <abigailrosea10@gmail.com>
2024-09-18 15:01:55 -0500

    accidental duplicates removed

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -82,7 +82,7 @@ class NeuralNetwork:
         dW1 = (1 / m) * np.dot(dZ1, X.T)
         db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
 
-        self.__W1 = self.__W1 - alpha * dW1
-        self.__b1 = self.__b1 - alpha * db1
-        self.__W2 = self.__W2 - alpha * dW2
-        self.__b2 = self.__b2 - alpha * db2
+        self.__W1 = alpha * dW1
+        self.__b1 = alpha * db1
+        self.__W2 = alpha * dW2
+        self.__b2 = alpha * db2
