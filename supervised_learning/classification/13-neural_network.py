b327bd326ac585f95cff4baf6bfa2edfad0a719d
aallm004 <abigailrosea10@gmail.com>
2024-09-18 15:54:59 -0500

    uff

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -58,7 +58,7 @@ class NeuralNetwork:
     def cost(self, Y, A):
         """calculates the cost of the model using logistic regression"""
         m = Y.shape[1]
-        cost = -1 / m * np.sum(
+        cost = (-1 / m) * np.sum(
             Y * np.log(A) + (1 - Y) *
             np.log(1.0000001 - A))
         return cost
