508b5265cd2be7cfdfe3ded181c1d29228ed4a26
aallm004 <abigailrosea10@gmail.com>
2024-09-18 15:51:00 -0500

    ahh

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -75,12 +75,12 @@ class NeuralNetwork:
         m = Y.shape[1]
 
         dZ2 = A2 - Y
-        dW2 = (1 / m) * np.dot(dZ2, A1.T)
-        db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
+        dW2 = 1 / m * np.dot(dZ2, A1.T)
+        db2 = 1 / m * np.sum(dZ2, axis=1, keepdims=True)
 
         dZ1 = np.dot(self.__W2.T, dZ2) * (A1 * (1 - A1))
-        dW1 = (1 / m) * np.dot(dZ1, X.T)
-        db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+        dW1 = 1 / m * np.dot(dZ1, X.T)
+        db1 = 1 / m * np.sum(dZ1, axis=1, keepdims=True)
 
         self.__W2 = alpha * dW2
         self.__b2 = alpha * db2
