d6feff56965ec18f1c710b8c3aa75faecada3580
aallm004 <abigailrosea10@gmail.com>
2024-09-18 18:04:14 -0500

    changed l to i

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class DeepNeuralNetwork:
     """class for DeepNeuralNetwork"""
-    
+
     def __init__(self, nx, layers):
         """Constructor"""
         if not isinstance(nx, int):
@@ -19,11 +19,13 @@ class DeepNeuralNetwork:
 
         self.cache = {}
         self.weights = {}
-        
-        for l in range(1, self.L + 1):
-            layer_size = layers[l - 1]
-            prev_layer_size = nx if l == 1 else layers[l - 2]
 
-            self.weights['W' + str(l)] = np.random.randn(layer_size, prev_layer_size) * np.sqrt(2 / prev_layer_size)
+        for i in range(1, self.L + 1):
+            layer_size = layers[i - 1]
+            prev_layer_size = nx if i == 1 else layers[i - 2]
+
+            self.weights['W' + str(l)] =
+            np.random.randn(layer_size, prev_layer_size) * np.sqrt
+            (2 / prev_layer_size)
 
             self.weights['b' + str(l)] = np.zeros((layer_size, 1))
