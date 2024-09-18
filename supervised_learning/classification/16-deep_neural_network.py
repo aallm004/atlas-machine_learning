b684d4a73571b1d6a419f1d26c56cb3b6f05bbea
aallm004 <abigailrosea10@gmail.com>
2024-09-18 17:32:30 -0500

    task 16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -5,24 +5,26 @@ import numpy as np
 
 class DeepNeuralNetwork:
     """class for DeepNeuralNetwork"""
+    
     def __init__(self, nx, layers):
         """Constructor"""
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0:
-            raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(layer, int) and layer > 0 for layer in layers):
+        if not isinstance(layers, list) or not layers:
             raise TypeError("layers must be a list of positive integers")
         
         self.L = len(layers)
+        layers.insert(0, nx)
+
         self.cache = {}
         self.weights = {}
         
-        for l in range(self.L):
-            if not isinstance(layers[l], int) or layers[l] <= 0:
-                raise TypeError("layers must be a list of positive integers")
-            he = np.random.randn(layers[l - 1], layers[l])
-            self.weights["W" + str(l)] = he
-            self.weights["b" + str(l)] = np.zeros((layers[l], 1))
+        for l in range(1, self.L + 1):
+            layer_size = layers[l - 1]
+            prev_layer_size = nx if l == 1 else layers[l - 2]
+
+            self.weights['W' + str(l)] = np.random.randn(layer_size, prev_layer_size) * np.sqrt(2 / prev_layer_size)
+
+            self.weights['b' + str(l)] = np.zeros((layer_size, 1))
