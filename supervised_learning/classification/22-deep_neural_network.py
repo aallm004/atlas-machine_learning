301c6bc56258121c082d32f382d36870b763ffd1
aallm004 <abigailrosea10@gmail.com>
2024-09-23 14:16:15 -0500

    <=

--- supervised_learning/classification/22-deep_neural_network.py
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -104,7 +104,7 @@ class DeepNeuralNetwork:
             raise ValueError("iterations must be a positive integer")
         if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
-        if alpha < 0:
+        if alpha <= 0:
             raise ValueError("alpha must be a positive float")
 
         for i in range(iterations):
