f47737d280da024b9ae3d33ad0556d4cbb2faf3a
aallm004 <abigailrosea10@gmail.com>
2024-09-23 10:44:15 -0500

    task 24

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""Module with one_hot_encode function"""
+import numpy as np
+
+
+def one_hot_encode(array):
+    """Converts a numeric label vector into a one-hot matrix"""
+    if not isinstance(array, np.array):
+        return None
+    if array.ndim < 2:
+        return None
+    
+    new_array = np.argmax(array, axis=0)
+    return new_array
