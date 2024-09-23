5f364ad0d129a9723c0c7f5d5fad7f3eb5396f8d
aallm004 <abigailrosea10@gmail.com>
2024-09-23 11:10:55 -0500

    task 25

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""Module containing the one_hot_decode function"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """Converts a one-hot matrix into a vector of labels"""
+
+    if not isinstance(one_hot, np.ndarray):
+        return None
+    if len(one_hot.shape) != 2:
+        return None
+    
+    try:
+        return np.argmax(one_hot, axis=0)
+    except Exception:
+        return None
