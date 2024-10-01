0e0fc98e7d385d3882103675bf589addbc0ec2ce
aallm004 <abigailrosea10@gmail.com>
2024-10-01 16:00:40 -0500

    adding missing import

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """tensorflow for beginners"""
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
+create_layer = __import__('1-create_layer').create_layer
+
 
 
 def forward_prop(x, layer_size=[] activations=[]):
