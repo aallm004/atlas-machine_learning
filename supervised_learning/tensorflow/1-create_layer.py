1843b553e4fed62995fa42b76293388230d5ef1c
aallm004 <abigailrosea10@gmail.com>
2024-10-01 15:52:32 -0500

    fixed overindentation

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -4,13 +4,13 @@ import tensorflow.compat.v1 as tf
 
 
 def create_layer(prev, n, activation):
-        i = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    i = tf.keras.initializers.VarianceScaling(mode='fan_avg')
 
-        layer = tf.keras.layers.Dense(
-            units=n,
-            activation=activation,
-            kernel_initializer=i,
-            name="layer"
-        )
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=i,
+        name="layer"
+    )
 
-        return layer(prev)
+    return layer(prev)
