8f18e6951a08f6499f4872a1955ba1157a06df48
aallm004 <abigailrosea10@gmail.com>
2024-10-01 16:02:36 -0500

    eh?

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -7,8 +7,7 @@ create_layer = __import__('1-create_layer').create_layer
 
 def forward_prop(x, layer_size=[] activations=[]):
     """function that creates the forward propagation graph for the neural network"""
+    prev = x
     for i in range(len(layer_size)):
-        if i == 0:
-            prev = x
         prev = create_layer(prev, layer_size[i], activations[i])
     return prev
