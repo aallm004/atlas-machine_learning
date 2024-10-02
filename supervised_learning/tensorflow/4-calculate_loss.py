f392471342a0c25e69676c601a12685063c8a408
aallm004 <abigailrosea10@gmail.com>
2024-10-02 11:28:07 -0500

    pycodestyle fix

--- supervised_learning/tensorflow/4-calculate_loss.py
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -2,6 +2,7 @@
 """tensorflow for beginners"""
 import tensorflow.compat.v1 as tf
 
+
 def calculate_loss(y, y_pred):
     """calculates the loss of prediction"""
     loss = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=y_pred)
