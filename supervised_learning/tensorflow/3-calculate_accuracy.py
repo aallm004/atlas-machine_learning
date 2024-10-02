dbc4617b97f9ef5c3a66a578bc568ae5005b5f5c
aallm004 <abigailrosea10@gmail.com>
2024-10-02 11:13:37 -0500

    task 3

---
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""tensorflow for beginners"""
+import tensorflow.compat.v1 as tf
+
+def calculate_accuracy(y, y_pred):
+    """calculates the accuracy of a prediction"""
+    prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+    accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
+    return accuracy
