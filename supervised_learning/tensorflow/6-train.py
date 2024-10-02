d47242b2b6290a911c742a6f6b73354127d46da5
aallm004 <abigailrosea10@gmail.com>
2024-10-02 15:12:59 -0500

    oops

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -3,8 +3,6 @@
 import tensorflow.compat.v1 as tf
 tf.disable_eager_execution()
 
-tf.set_random_seed(1234)
-
 create_placeholders = __import__('0-create_placeholders').create_placeholders
 forward_prop = __import__('2-forward_prop').forward_prop
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
@@ -13,7 +11,6 @@ create_train_op = __import__('5-create_train_op').create_train_op
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     """builds, trains, and saves a neural network classifier"""
-    # Create the neural network
     def build_graph():
         """build graph function"""
         x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
@@ -23,16 +20,6 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
         train_op = create_train_op(loss, alpha)
         return x, y, y_pred, loss, accuracy, train_op
 
-    def print_metrics(iteration, train_cost, train_accuracy, valid_cost, valid_accuracy):
-        """print metrics function"""
-        print(f"After {iteration} iterations:")
-        print(f"\tTraining Cost: {train_cost}")
-        print(f"\tTraining Accuracy: {train_accuracy}")
-        print(f"\tValidation Cost: {valid_cost}")
-        print(f"\tValidation Accuracy: {valid_accuracy}")
-
-
-    # Call build_graph and unpack its return values
     x, y, y_pred, loss, accuracy, train_op = build_graph()
 
     # Add tensors to collection
@@ -42,7 +29,7 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
     tf.add_to_collection('loss', loss)
     tf.add_to_collection('accuracy', accuracy)
     tf.add_to_collection('train_op', train_op)
-
+    
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
 
@@ -53,8 +40,12 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
             train_cost, train_accuracy = sess.run([loss, accuracy], feed_dict={x: X_train, y: Y_train})
             valid_cost, valid_accuracy = sess.run([loss, accuracy], feed_dict={x: X_valid, y: Y_valid})
 
-            if i % 100 == 0  or i == iterations:
-                print_metrics(i, train_cost, train_accuracy, valid_cost, valid_accuracy)
+            if i % 100 == 0 or i == iterations:
+                print(f"After {i} iterations:")
+                print(f"\tTraining Cost: {train_cost}")
+                print(f"\tTraining Accuracy: {train_accuracy}")
+                print(f"\tValidation Cost: {valid_cost}")
+                print(f"\tValidation Accuracy: {valid_accuracy}")
 
             if i < iterations:
                 sess.run(train_op, feed_dict={x: X_train, y: Y_train})
@@ -62,5 +53,3 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
         save_path = saver.save(sess, save_path)
 
     return save_path
-
-
