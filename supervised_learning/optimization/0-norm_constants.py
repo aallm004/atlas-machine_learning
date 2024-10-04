7ee1d0e473bc3e4286af93a26e2aec8461393244
aallm004 <abigailrosea10@gmail.com>
2024-10-04 14:48:20 -0500

    tasks 0 and 1

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""documentation"""
+import numpy as np
+import tensorflow as tf
+
+
+def normalization_constants(X):
+    """
+    Calculates the normalization constants of a matrix
+    
+    X is the numpy.ndarray or shape (m, nx) to normalize
+        m is the number of data points
+        nx is the number of features
+
+    Returns: the mean and standard deviation of each feature, respectively    
+    """
+    mean = np.mean(X, axis=0)
+
+    std = np.std(X, axis=0)
+
+    return mean, std
