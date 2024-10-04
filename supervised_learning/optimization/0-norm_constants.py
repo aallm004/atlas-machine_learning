15ac00a75a0e4e892fc1502c368b4379d3305aba
aallm004 <abigailrosea10@gmail.com>
2024-10-04 14:59:14 -0500

    pycodestyle

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -6,19 +6,20 @@ import tensorflow as tf
 def normalization_constants(X):
     """
     Calculates the normalization constants of a matrix
-    
+
     X is the numpy.ndarray or shape (m, nx) to normalize
         m is the number of data points
         nx is the number of features
 
-    Returns: the mean and standard deviation of each feature, respectively    
+    Returns: the mean and standard deviation of each feature, respectively
     """
     m = len(X)
     nx = len(X[0])
 
     mean = [sum(X[i][j] for i in range(m)) / m for j in range(nx)]
 
-    var = [sum((X[i][j] - mean[j])**2 for i in range(m)) / m for j in range(nx)]
+    var = [sum((X[i][j] - mean[j])**2 for i in range(m)) /
+           m for j in range(nx)]
 
     std = [var[j]**0.5 for j in range(nx)]
 
