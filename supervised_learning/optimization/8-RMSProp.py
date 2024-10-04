d9dbe700dfbf57093648bd154e88a69af48a0734
aallm004 <abigailrosea10@gmail.com>
2024-10-04 17:05:19 -0500

    task 8

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""documentation"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """sets up the RMSProp optimization algorithm in Tensorflow
+    alpha is the learning rate
+    beta2 is the RMSProp weight (discounting factor)
+    epsilon is a small number to avoid division by zero
+
+    Returns: optimizer
+    """
+
+    optimizer = tf.train.RMSPropOptimizer(
+        learning_rate=alpha,
+        decay=beta2,
+        epsilon=epsilon
+    )
+
+    return optimizer
