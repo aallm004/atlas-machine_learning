20356978e9cd60550c157b120083fb16c528c5fb
aallm004 <abigailrosea10@gmail.com>
2024-10-04 16:14:32 -0500

    task 4

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -4,4 +4,17 @@ import numpy as np
 
 
 def moving_average(data, beta):
-    """F"""
+    """Calculates the weighted moving average of data set
+
+    data is the list of data to calculate the moving average of
+    beta is the weight used for the moving average
+    moving average calculatoin uses bias correction
+
+    Returns: list containing the moving averages of data
+    """
+    weighted_average = 0
+    moving_average = []
+    for x, i in enumerate(data, 1):
+        weighted_average = beta * weighted_average + (1 - beta) * i
+        moving_average.append(weighted_average / (1 - beta**i))
+    return moving_average
