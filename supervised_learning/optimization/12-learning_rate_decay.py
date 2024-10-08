0ed5c1ae4e8ab0933884f47bf9c33143e8e1b25a
aallm004 <abigailrosea10@gmail.com>
2024-10-08 16:36:41 -0500

    pycodestyle

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -13,7 +13,6 @@ def learning_rate_decay(alpha, decay_rate, decay_step):
     the learning rate decay should occur ina stepwise fashion
 
     Returns: the learning rate decay operation
-    
     """
     return tf.keras.optimizers.schedules.InverseTimeDecay(
         initial_learning_rate=alpha,
