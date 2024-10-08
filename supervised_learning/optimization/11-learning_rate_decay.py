20918cec2d7515963ecf3b7d9e3b94d4c926247d
aallm004 <abigailrosea10@gmail.com>
2024-10-08 16:33:17 -0500

    tasks 12 and 13

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""documentation"""
+import numpy as np
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """updates the learning rate using inverse time decay
+    alpha is the original learning rate
+    decay_rate determines the rate at which alpha decays
+    global_step is the # of passes of gradient descent that
+    have elapsed
+    decay_step is the # of passes of GD that should occur
+    before alpha is decayed further
+    """
+    return alpha / (1 + decay_rate * np.floor(global_step / decay_step))
