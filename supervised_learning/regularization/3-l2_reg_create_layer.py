f76e22faabdd55cc44f1b4bd890c46780da0031a
aallm004 <abigailrosea10@gmail.com>
2024-10-16 18:31:38 -0500

    pycoderstylers

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -13,7 +13,8 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
         lambtha is the L2 regularization parameter
         Returns: the output of the new layer
     """
-    initialize = tf.keras.initializers.VarianceScaling(scale=2.0, mode=("fan_avg"))
+    initialize = tf.keras.initializers.\
+        VarianceScaling(scale=2.0, mode=("fan_avg"))
     regularize = tf.keras.regularizers.l2(lambtha)
     layer = tf.keras.layers.Dense(
         n,
