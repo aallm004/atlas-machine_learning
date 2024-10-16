f21d1290459619d63062c8342e08a1925b9416d8
aallm004 <abigailrosea10@gmail.com>
2024-10-16 18:03:30 -0500

    eh

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -2,25 +2,29 @@
 """documentation"""
 import tensorflow as tf
 
+
 def l2_reg_cost(cost, model):
     """
     Calculate the cost of a neural network with L2 regularization.
 
     Args:
     cost (tensor): The cost of the network without L2 regularization
-    model (keras.Model): A Keras model that includes layers with L2 regularization
+    model (keras.Model): A Keras model that includes layers with L2
+    regularization
 
     Returns:
-    tensor: The total cost for each layer of the network, accounting for L2 regularization
+    tensor: The total cost for each layer of the network, accounting for L2
+    regularization
     """
     cost_l2 = []
     for layer in model.layers:
         if hasattr(layer, 'kernel') and layer.kerel_regularizer:
-            cost_l2 += tf.math.reduce_sum(layer.kernel_regularizer(layer.kernel))
+            cost_l2 += \
+                tf.math.reduce_sum(layer.kernel_regularizer(layer.kernel))
 
         if hasattr(layer, 'bias_regularizer') and layer.bias_regularizer:
             l2_cost += tf.math.reduce_sum(layer.bias_regularizer(layer.bias))
 
     total_cost = cost + l2_cost
-    
+
     return total_cost
