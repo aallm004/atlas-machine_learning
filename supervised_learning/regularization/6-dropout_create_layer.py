96257513d95a46577c8b8f79346e86672e2d904d
aallm004 <abigailrosea10@gmail.com>
2024-10-16 19:06:18 -0500

    task 6

---
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""documentation"""
+import tensorflow as tf
+
+
+def dropout_create_layer(prev, n, activation, keep_prob,training=True):
+    """
+    Function that creates a layer of a neural network using dropout
+        prev is the previous layer
+        n is the number of nodes in the new layer
+        activation is the activation function to be used
+        keep_prob is the probability that a node will be kept
+        training is a boolean indicating whether the layer is in training mode
+        Returns: the output of the new layer
+    """
+        # Initialize weights and biases
+    initializer = tf.keras.initializers.GlorotUniform()
+    W = tf.Variable(initializer(shape=(prev.shape[1], n)), name="W")
+    b = tf.Variable(tf.zeros(shape=(1, n)), name="b")
+    
+    # Compute the layer output
+    Z = tf.matmul(prev, W) + b
+    A = activation(Z)
+    
+    # Apply dropout only during training
+    if training:
+        A = tf.nn.dropout(A, rate=1 - keep_prob)
+    
+    return A
