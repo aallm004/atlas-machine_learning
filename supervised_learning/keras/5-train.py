b7cb42506ed26cdcfbe519a2eec598d01d773168
aallm004 <abigailrosea10@gmail.com>
2024-10-21 17:37:16 -0500

    forgot to fix import

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import numpy as np
-import tensorflow as tf
+"""documentation"""
+import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
