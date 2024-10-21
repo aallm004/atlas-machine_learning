dc3ea7816d936e224bb7ca51fa4c8b14e3c2896d
aallm004 <abigailrosea10@gmail.com>
2024-10-21 16:19:06 -0500

    pycodestlye

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -6,7 +6,7 @@ import tensorflow.keras as K
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """Function that builds a neural network with the keras libray
        nx is the number of input features to the network
-    layers is a list containing the number of nodes in each layer of the 
+    layers is a list containing the number of nodes in each layer of the
     network
     activations is a list containing the activation functions used for each
     layer of the network
