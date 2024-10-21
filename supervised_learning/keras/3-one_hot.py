a1a12d3f3734364d4b51ec181b9d6c66eea7362b
aallm004 <abigailrosea10@gmail.com>
2024-10-21 17:24:09 -0500

    somehow pycodestyle

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+"""documentation"""
 import tensorflow.keras as K
 
 
 def one_hot(labels, classes=None):
     """function that conerts a label vector into a one-hot matrix
         The last dimension of the one-hot matrix must be the number of classes
-        
+
         Returns: one-hot matrix
     """
     return K.utils.to_categorical(labels, num_classes=classes)
