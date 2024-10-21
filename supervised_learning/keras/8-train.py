9cce74e01669ccd7befa80cd3195c35bdb89d9f4
aallm004 <abigailrosea10@gmail.com>
2024-10-21 18:00:43 -0500

    task 8

--- supervised_learning/keras/8-train.py
+++ supervised_learning/keras/8-train.py
@@ -1,3 +1,51 @@
 #!/usr/bin/env python3
-import numpy as np
-import tensorflow as tf
+"""documentation"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                save_best=False, filepath=None, verbose=True, shuffle=False):
+    """Function that trains a model using mini-batch gradient descent
+    network is the model to train
+    data is a numpy.ndarray of shape(m, nx) containing the input data
+    labels is a one-hot numpy.ndarray of shape(m, classes) containing the
+    labels of data
+    batch_size is the size of the batch used for mini-batch gradient descent
+    epochs is the number of passes through data for mini-batch gradient
+    descent
+    verbose is a boolean that determines if output should be printed during
+    training
+    shuffle is a boolean that determines if the data should be shuffled after
+    each epoch
+
+    Returns: the History object generated after training the model
+    """
+    callbacks = []
+    if early_stopping and validation_data:
+        callbacks.append(K.callbacks.EarlyStopping(
+            monitor='val_loss', patience=patience))
+
+    if learning_rate_decay and validation_data:
+        def lr_schedule(epoch):
+            return alpha / (1 + decay_rate * epoch)
+
+        callbacks.append(K.callbacks.LearningRateScheduler(lr_schedule,
+
+    if save_best and filepath and validation_data:
+        callbacks.append(K.callbacks.ModelCheckpoint(filepath,
+           save_best_only=True, monitor='val_loss', mode='min'))                                                                             verbose=1))
+
+    K.backend.set_value(network.optimizer.learning_rate, alpha)
+
+    return network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=validation_data,
+        verbose=verbose,
+        shuffle=shuffle,
+        callbacks=callbacks
+    )
