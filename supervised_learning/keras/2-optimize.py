a485bf98793951cbe47f3248abfa1fd97df41e2d
aallm004 <abigailrosea10@gmail.com>
2024-10-21 17:20:01 -0500

    pycodestyle

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -10,14 +10,15 @@ def optimize_model(network, alpha, beta1, beta2):
     alpha is the learning rate
     beta1 is the first Adam optimization parameter
     beta2 is the second Adam optimization parameter
-    
+
     Returns: None
     """
     # Set the loss function
     network.compile(loss='categorical_crossentropy', optimizer='adam')
-    
+
     # Create and set the Adam optimizer with custom parameters
-    optimizer = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
+    optimizer = K.optimizers.Adam(
+        learning_rate=alpha, beta_1=beta1, beta_2=beta2)
     network.optimizer = optimizer
-    
+
     return None
