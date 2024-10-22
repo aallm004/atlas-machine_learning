e1194c6dfe6eaeaa55805116b4fa5319da1775eb
aallm004 <abigailrosea10@gmail.com>
2024-10-22 15:05:28 -0500

    pycodestyle

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -26,6 +26,7 @@ def convolve_grayscale_valid(images, kernel):
 
     for i in range(output_h):
         for j in range(output_w):
-            output[:, i, j] = mp.sum(images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
+            output[:, i, j] = mp.sum(images[:, i:i+kh, j:j+kw] * kernel,
+                                     axis=(1, 2))
 
     return output
