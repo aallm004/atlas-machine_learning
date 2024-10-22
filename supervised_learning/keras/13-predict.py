8f57c4132b6cab3c45a386e4943573e34918253c
aallm004 <abigailrosea10@gmail.com>
2024-10-22 14:07:37 -0500

    meh

--- supervised_learning/keras/13-predict.py
+++ supervised_learning/keras/13-predict.py
@@ -7,7 +7,8 @@ def predict(network, data, verbose=False):
     """ Function that makes a prediction using a neural network
     network is the network model to make the prediction with
     data is the input data to make the prediction with
-    verbose is a boolean that determines if output should be printed during the prediction process
+    verbose is a boolean that determines if output should be printed during
+    the prediction process
     Returns: the prediction for the data"""
 
     return network.prediction(data, verbose=verbose)
