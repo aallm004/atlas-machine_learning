7130f591daf7926c341678bb28cdf5f7af31a48c
aallm004 <abigailrosea10@gmail.com>
2024-10-22 15:08:47 -0500

    changed mistaken mp to np

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -21,7 +21,7 @@ def convolve_grayscale_valid(images, kernel):
     kh, kw = kernel.shape
     output_h = h - kh + 1
     output_w = w - kw + 1
-    output = mp.zeros((m, output_h, output_w))
+    output = np.zeros((m, output_h, output_w))
 
     for i in range(output_h):
         for j in range(output_w):
