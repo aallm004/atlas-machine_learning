c03df841f1dab484c6c8b5c02e4bafec2dabf6f6
aallm004 <abigailrosea10@gmail.com>
2024-10-22 14:05:33 -0500

    task 13

--- supervised_learning/keras/13-predict.py
+++ supervised_learning/keras/13-predict.py
@@ -3,23 +3,11 @@ import numpy as np
 import tensorflow as tf
 
 
-def build_model(nx, layers, activations, lambtha, keep_prob):
-    """Function that builds a neural network with the keras library.
-    nx is the number of input features to the network
-    layers is a list ocontaining the number of nodes in each layer of the
-    network
-    activations is a list containing the activation functions used for each
-    layer of the network
-    lambtha is the L2 regularization parameter
-    keep_prob is the probability that a node will be kept for dropout
-    
-    Returns: the keras model"""
-    inputs = tf.keras.Input(shape=(nx,))
-    x = inputs
-    for i in range(len(layers)):
-        x = tf.keras.layers.Dense(layers[i], activation=activations[i],
-                                  kernel_regularizer=tf.keras.regularizers.l2(lambtha))(x)
-        if i < len(layers) - 1:
-            x = tf.keras.layers.Dropout(1 - keep_prob)(x)
-    model = tf.keras.Model(inputs=inputs, outputs=x)
-    return model
+def predict(network, data, verbose=False):
+    """ Function that makes a prediction using a neural network
+    network is the network model to make the prediction with
+    data is the input data to make the prediction with
+    verbose is a boolean that determines if output should be printed during the prediction process
+    Returns: the prediction for the data"""
+
+    return network.prediction(data, verbose=verbose)
