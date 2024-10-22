b8b6b7aa4baf0b83ca0e4a7957c29fbdac5f9e84
aallm004 <abigailrosea10@gmail.com>
2024-10-22 15:51:03 -0500

    forgot to close a p

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -31,8 +31,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     sh, sw = stride
 
     if padding == 'same':
-        ph = int(np.ceil((h - 1) * sh + kh - h) // 2
-        pw = int(np.ceil((w - 1) * sw + kw - w) // 2))
+        ph = int(np.ceil((h - 1) * sh + kh - h) // 2)
+        pw = int(np.ceil((w - 1) * sw + kw - w) // 2)
     elif padding == 'valid':
         ph, pw = 0, 0
     else:
