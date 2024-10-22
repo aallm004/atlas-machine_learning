6e071c555d9ce65a9b0967a503f580f9dcc618de
aallm004 <abigailrosea10@gmail.com>
2024-10-22 13:27:01 -0500

    task 9

--- supervised_learning/keras/9-model.py
+++ supervised_learning/keras/9-model.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
-import numpy as np
-import tensorflow as tf
+"""documentation"""
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """Function that saves an entire model
+        network is the model to save
+        filename is the path of the file that the model should be saved to
+        Returns: None
+    """
+    network.save(filename)
+
+
+def load_model(filename):
+    """Function that loads and entire model
+    filename is the path of the file that the model should be loaded from
+    Returns: the loaded model
+    """
+    return K.models.load_model(filename)
