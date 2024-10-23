e60e4f402744a6019a5c6705a4facb4603601ad4
aallm004 <abigailrosea10@gmail.com>
2024-10-23 12:55:17 -0500

    pycoooodestyles

--- math/convolutions_and_pooling/6-pool.py
+++ math/convolutions_and_pooling/6-pool.py
@@ -5,7 +5,8 @@ import numpy as np
 
 def pool(images, kernel_shape, stride, mode='max'):
     """Function that performs pooling on images
-        IMAGES is a numpy.ndarray with shape (m, h, w, c) containing multiple images
+        IMAGES is a numpy.ndarray with shape (m, h, w, c) containing
+        multiple images
             m is the number of images
             h is the height in pixels of the images
             w is the width in pixels of the images
