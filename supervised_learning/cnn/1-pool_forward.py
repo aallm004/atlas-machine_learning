e8c2031645c49dcc4ec40fcac5252a7d8a39131d
aallm004 <abigailrosea10@gmail.com>
2024-10-29 13:52:18 -0500

    pycodestlye and changing an "image" to A_prev

--- supervised_learning/cnn/1-pool_forward.py
+++ supervised_learning/cnn/1-pool_forward.py
@@ -4,24 +4,28 @@ import numpy as np
 
 
 def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
-    """Function that performs forward propogation over a pooling layer of a neural network
-        A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing the output of the previous layer
+    """Function that performs forward propogation over a pooling layer of a
+    neural network
+        A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+        containing the output of the previous layer
             m is the number of examples
             h_prev is the height of the previous layer
             w_prev is the width of the previous layer
             c_prev is the number of channels in the previous layer
-        kernal_shape is a tuple of (kh, kw) containing the size of the kernel for the pooling
+        kernal_shape is a tuple of (kh, kw) containing the size of the kernel
+        for the pooling
             kh is the kernel height
             kw is the kernel width
         stride is a tuple of (sh, sw) containing the strides for the pooling
             sh is the stride for the height
             sw is the stride for the width
-        mode is a string containing either max or avg, indicating whether to perform maximum or average pooling, respectively
+        mode is a string containing either max or avg, indicating whether to
+        perform maximum or average pooling, respectively
 
         Returns: the output of the pooling layer
         """
-    
-    m, h, w, c = images.shape
+
+    m, h, w, c = A_prev.shape
     kh, kw = kernel_shape
     sh, sw = stride
 
