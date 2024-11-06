7177b1b5ec64bd41f03a090697d9279fc24fb373
aallm004 <abigailrosea10@gmail.com>
2024-11-06 13:31:00 -0600

    checking if error is in comments

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -4,4 +4,16 @@ from tensorflow import keras as K
 
 
 def identity_block(A_prev, filters):
+    """Function that builds an identity block
+        A_prev is the output from the previous layer
+        filters is a tuple of list containing F11, F3, F12
+            F11 is the number of filters in the first 1x1 convolution
+            F3 is the number of filters in the 3x3 convolution
+            F12 is the number of filters in the second 1x1 convolution
+        All convolutions inside the block should be followed by batch
+        normalization along the channels axis and a relu activation
+        All weights should use he normal initialization
+        The seeed for the he_normal initializer should be set to zero
+        
+        Returns: the activated output of the indentity block"""
     pass
