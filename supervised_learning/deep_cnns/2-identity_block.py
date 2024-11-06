7f813ac6808870f3ced651413781a37c89e62824
aallm004 <abigailrosea10@gmail.com>
2024-11-06 13:31:54 -0600

    checking comments for error again

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -5,7 +5,7 @@ from tensorflow import keras as K
 
 def identity_block(A_prev, filters):
     """Function that builds an identity block
-        A_prev is the output from the previous layer
+        A_prev is the output the previous layer
         filters is a tuple of list containing F11, F3, F12
             F11 is the number of filters in the first 1x1 convolution
             F3 is the number of filters in the 3x3 convolution
