9f590b938a86cd0a675401cf56b8cc4ccf72dc2c
aallm004 <abigailrosea10@gmail.com>
2024-11-27 14:47:16 -0600

    pycodestyle

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -4,17 +4,27 @@ from tensorflow import keras as K
 
 
 class Yolo:
-    """Class Yolo that uses the Yolo v3 algorithm to perform object detection"""
-    
+    """Class Yolo that uses the Yolo v3 algorithm to perform object
+    detection"""
+
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """Class Constructor:
                 model_path: the path to where a Darknet Keras model is stored
-                classes_path: the path to where the list of class names used for the Darknet model, listed in order of index, can be found
-                class_t: a float representing the box score threshold for the initial filtering step
-                nms_t: a float representing the IOU threshold for non-max suppression
-                anchors: a numpy.ndarray of shape (outputs, anchor_boxes, 2) containing all of the anchor boxes:
-                    outputs: the number of outputs (predictions) made by the Darknet model
-                    anchor_boxes: the number of anchor boxes used for each prediction
+
+                classes_path: the path to where the list of class names used
+                for the Darknet model, listed in order of index, can be found
+
+                class_t: a float representing the box score threshold for the
+                initial filtering step
+
+                nms_t: a float representing the IOU threshold for non-max
+                suppression
+                anchors: a numpy.ndarray of shape (outputs, anchor_boxes, 2)
+                containing all of the anchor boxes:
+                    outputs: the number of outputs (predictions) made by the
+                    Darknet model
+                    anchor_boxes: the number of anchor boxes used for each
+                    prediction
                     2 => [anchor_nox_width, anchor_box_height]
             Public instance attributes:
                 model: the Darknet Keras model
