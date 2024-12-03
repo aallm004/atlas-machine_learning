44d6d6288a905138447b0462495d65e09499dacb
aallm004 <abigailrosea10@gmail.com>
2024-12-03 15:08:59 -0600

    WHat even

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -26,33 +26,49 @@ class Yolo:
         for idx, output in enumerate(outputs):
             grid_height, grid_width, anchors_count = output.shape[:3]
             
-            # Extract confidence and class probabilities
+            # Get confidence and class probabilities
             box_confidences.append(1 / (1 + np.exp(-output[..., 4:5])))
             box_class_probs.append(1 / (1 + np.exp(-output[..., 5:])))
 
-           # Process box coordinates
+            # Process box coordinates
             box = np.zeros(output[..., :4].shape)
             
-            # Center coordinates
-            box[..., 0] = (1 / (1 + np.exp(-output[..., 0])) + 
-                          np.tile(np.arange(grid_width), grid_height).reshape(grid_height, grid_width, 1))
-            box[..., 1] = (1 / (1 + np.exp(-output[..., 1])) + 
-                          np.tile(np.arange(grid_height), grid_width).reshape(grid_width, grid_height).T.reshape(grid_height, grid_width, 1))
+            # Create grid
+            grid_x = np.arange(grid_width)
+            grid_x = np.tile(grid_x, grid_height)
+            grid_x = grid_x.reshape(grid_height, grid_width, 1)
             
-            # Width and height
-            box[..., 2] = np.exp(output[..., 2]) * self.anchors[idx, :, 0]
-            box[..., 3] = np.exp(output[..., 3]) * self.anchors[idx, :, 1]
-
-            box[..., 0] *= image_size[1] / grid_width
-            box[..., 1] *= image_size[0] / grid_height
-            box[..., 2] *= image_size[1] / grid_width
-            box[..., 3] *= image_size[0] / grid_height
-
-            box[..., 0] -= box[..., 2] / 2
-            box[..., 1] -= box[..., 3] / 2
-            box[..., 2] += box[..., 0]
-            box[..., 3] += box[..., 1]
-
+            grid_y = np.arange(grid_height)
+            grid_y = np.tile(grid_y, grid_width)
+            grid_y = grid_y.reshape(grid_width, grid_height).T
+            grid_y = grid_y.reshape(grid_height, grid_width, 1)
+            
+            # Box centers
+            box[..., 0] = (1 / (1 + np.exp(-output[..., 0]))) + grid_x
+            box[..., 1] = (1 / (1 + np.exp(-output[..., 1]))) + grid_y
+            
+            # Box width and height
+            anchors = self.anchors[idx].reshape(1, 1, anchors_count, 2)
+            box[..., 2] = np.exp(output[..., 2]) * anchors[..., 0]
+            box[..., 3] = np.exp(output[..., 3]) * anchors[..., 1]
+            
+            # Convert to corner coordinates
+            box[..., 0] = box[..., 0] / grid_width
+            box[..., 1] = box[..., 1] / grid_height
+            box[..., 2] = box[..., 2] / grid_width
+            box[..., 3] = box[..., 3] / grid_height
+            
+            # Scale to image size
+            box[..., 0] *= image_size[1]
+            box[..., 1] *= image_size[0]
+            box[..., 2] *= image_size[1]
+            box[..., 3] *= image_size[0]
+            
+            # Convert to boundary box format
+            box_mins = box[..., 0:2] - (box[..., 2:4] / 2)
+            box_maxs = box[..., 0:2] + (box[..., 2:4] / 2)
+            box = np.concatenate([box_mins, box_maxs], axis=-1)
+            
             boxes.append(box)
 
         return boxes, box_confidences, box_class_probs
