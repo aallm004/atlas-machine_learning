3b02e09bc282d852f1228c8ac3fdbd268c3cddd8
aallm004 <abigailrosea10@gmail.com>
2024-12-12 10:52:54 -0600

    converting from bgr to rgb

--- supervised_learning/object_detection/5-yolo.py
+++ supervised_learning/object_detection/5-yolo.py
@@ -63,6 +63,8 @@ class Yolo:
                                 (input_w, input_h),
                                 interpolation=cv2.INTER_CUBIC)
             
+            resized = cv2.ctvColor(resized, cv2.COLOR_BGR2RGB)
+            
             preprocessed = resized.astype(np.float32) / 255.0
 
             pimages.append(preprocessed)
