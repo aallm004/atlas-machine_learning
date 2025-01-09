74f84d31d29960131737da7a58173cc44e9bf18f
aallm004 <abigailrosea10@gmail.com>
2025-01-09 13:49:28 -0600

    pycodestlye corrections

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -2,6 +2,7 @@
 """module 1"""
 determinant = __import__('0-determinant').determinant
 
+
 def minor(matrix):
     """function that calculates the minor matrix of a matrix
         matrix: list of lists whose minor matrix should be calculated
@@ -13,24 +14,23 @@ def minor(matrix):
     n = len(matrix)
     if not all(len(row) == n for row in matrix) or matrix == [[]]:
         raise ValueError("matrix must be a non-empty square matrix")
-    
+
     new_matrix = [[0 for _ in range(n)] for _ in range(n)]
-    #for _ in range(n):
-     #   new_matrix.append([None] * n)
 
     for r in range(n):
         for c in range(n):
-            #print(n, r, c, new_matrix)
             x = sub_matrix(matrix, r, c)
             if x == [[]]:
                 return [[1]]
             new_matrix[r][c] = determinant(x)
     return new_matrix
 
+
 def sub_matrix(matrix, r, c):
+    """creating a sub matrix"""
     n = len(matrix)
     if n == 1:
         return [[]]
     # Minor matrix excluding first row and current column
     return [[matrix[i][k] for k in range(n) if k != c]
-                for i in range(n) if i != r]
+            for i in range(n) if i != r]
