3b1b6600d8dac2e9231ef6984cfcaf5562aa20e5
aallm004 <abigailrosea10@gmail.com>
2025-01-09 14:36:22 -0600

    task 3

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""module 3"""
+cofactor = __import__('2-cofactor').cofactor
+
+
+def adjugate(matrix):
+    """function that calcultes the adjugate matrix of a matrix"""
+    cofactor_matrix = cofactor(matrix)
+    n = len(matrix)
+    result = [[0 for _ in range(n)] for _ in range(n)]
+    for r in range(n):
+        for c in range(n):
+            result[c][r] = cofactor_matrix[r][c]
+    return result
