01c871d5ad517640ace6a57d8d0c7b0df43c443a
aallm004 <abigailrosea10@gmail.com>
2025-01-12 14:02:54 -0600

    redundancy

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -27,4 +27,4 @@ class MultiNormal:
         center = data - self.mean
 
         # Calculate covariance matrix
-        self.cov = (1 / n) * np.dot(center, center.T) / n
+        self.cov = np.dot(center, center.T) / n
