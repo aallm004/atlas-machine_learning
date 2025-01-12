92dd69fd54e131085153c30612e5e464042101f5
aallm004 <abigailrosea10@gmail.com>
2025-01-12 13:28:14 -0600

    C.shape

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -9,8 +9,8 @@ def correlation(C):
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
 
-    if len(C) != 2 or C.shape[0] != C.shape[1]:
-        raise ValueError("C must be a 2D square matrix")
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2 square matrix")
 
     diag = np.diagonal(C)
 
