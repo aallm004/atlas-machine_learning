8cb85b8128ac8fdc347a42f0877887cdccaed940
aallm004 <abigailrosea10@gmail.com>
2025-01-12 13:22:06 -0600

    fixed error message

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
+"""module 1"""
 import numpy as np
+
+
+def correlation(C):
+    """function that calculates a correlation matrix"""
+    
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+
+    if len(C) != len(C[0]):
+        raise ValueError("C must be a 2d square matrix")
+
+    diag = np.diagonal(C)
+
+    std_dev = np.sqrt(diag)
+
+    correlation = C / np.outer(std_dev, std_dev)
+
+    return correlation
