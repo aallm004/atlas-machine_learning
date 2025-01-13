30f6b8e95a987bbfc7f90e3f0dc0fdd77132fbbe
aallm004 <abigailrosea10@gmail.com>
2025-01-12 18:00:26 -0600

    pycodestyle

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -4,12 +4,12 @@ import numpy as np
 
 def likelihood(x, n, P):
 
-
-    if not isinstance(n, int)or n <= 0:
+    if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
 
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError("x must be an integer that is greater than or equal \
+                         to 0")
 
     if x > n:
         raise ValueError("x cannot be greater than n")
@@ -20,7 +20,8 @@ def likelihood(x, n, P):
     if not np.all((P >= 0) & (P <= 1)):
         raise ValueError("All values in P must be in the range [0, 1]")
 
-    ant = np.math.factorial(n) / (np.math.factorial(x) * np.math.factorial(n - x))
+    ant = np.math.factorial(n) / (np.math.factorial(x) *
+                                  np.math.factorial(n - x))
 
     like = ant * (P ** x) * ((1 - P) ** (n - x))
 
