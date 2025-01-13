9f4a0ae5279b32c5da7fcc6d20e533116826e30f
aallm004 <abigailrosea10@gmail.com>
2025-01-13 15:39:19 -0600

    moved likelihood in the house

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import numpy as np
-likelihood = __import__('0-likelihood').likelihood
 
 
 def intersection(x, n, P, Pr):
@@ -31,8 +30,38 @@ def intersection(x, n, P, Pr):
     if not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
 
-    l = likelihood(x, n, P)
 
-    intersect = l * Pr
+    like = likelihood(x, n, P)
+
+    intersect = like * Pr
 
     return intersect
+
+
+def likelihood(x, n, P):
+    """function that calculates the likelihood of obtaining this data given
+    various hypothetical probabiliies"""
+
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is greater than or " +
+                         "equal to 0")
+
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    ant = np.math.factorial(n) / (np.math.factorial(x) *
+                                  np.math.factorial(n - x))
+
+    like = ant * (P ** x) * ((1 - P) ** (n - x))
+
+    return like
+
