e50272bc38b7d076d9abc3be0fc8d45316b34cca
aallm004 <abigailrosea10@gmail.com>
2025-01-13 15:42:35 -0600

    task 1

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""module for intersection"""
 import numpy as np
 
 
@@ -10,7 +11,8 @@ def intersection(x, n, P, Pr):
         raise ValueError("n must be a positive integer")
 
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError("x must be an integer that is greater than or equal +
+                         to 0")
 
     if x > n:
         raise ValueError("x cannot be greater than n")
@@ -23,13 +25,12 @@ def intersection(x, n, P, Pr):
 
     if not np.all((P >= 0) & (P <= 1)):
         raise ValueError("All values in P must be in the range [0, 1]")
-    
-    if not np.all((Pr >= 0) & (Pr <=1)):
+
+    if not np.all((Pr >= 0) & (Pr <= 1)):
         raise ValueError("All values in Pr must be in the range [0, 1]")
 
     if not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
-
 
     like = likelihood(x, n, P)
 
@@ -64,4 +65,3 @@ def likelihood(x, n, P):
     like = ant * (P ** x) * ((1 - P) ** (n - x))
 
     return like
-
