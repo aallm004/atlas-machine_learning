cff90e8633e8b38f9f699d31bc59ab0a1bf41c84
aallm004 <abigailrosea10@gmail.com>
2025-01-18 11:35:29 -0600

    task 1

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+from numpy import np
+
+
+def pca(X, var=0.95):
+    """function that performs PCA o a dataset
+        X is a numpy.ndarray of shape (n, d) where:
+            n is the number of data points
+            d is the number of dimenstions in each point
+        var is the fraction of the variance that the PCA transformation should
+        maintain
+        
+        Returns: the weights matrix, W, that maintains var fraction of X's
+        original variance
+            W is a numpy.ndarray of shape (d, nd) where nd is the new
+            dimensionality of the transformed X
+        """
+    # Dimensions
+    n, d = X.shape
+
+    # Calculate covariance matrix (d x d)
+    covariance = np.cov(X, rowvar=False)
+
+    
