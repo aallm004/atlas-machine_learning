ad5a725bbb92a40ff08db0c92da90a8d9e17f6fa
aallm004 <abigailrosea10@gmail.com>
2025-01-22 17:52:20 -0600

    fack

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -20,7 +20,7 @@ def expectation(X, pi, m, S):
             l is the total log likelihood"
     
     """
-    print(f'{X}, {pi}, {m}, {f}')
+    print(f'{X}, {pi}, {m}, {S}')
     try:
 
         # Get dimensions
