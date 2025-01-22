421baf2ba4aabc3876d1c8d73a5533df44291f1d
aallm004 <abigailrosea10@gmail.com>
2025-01-22 17:29:58 -0600

    for to For

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
-"""Module for expectation"""
+"""Module For expectation"""
 import numpy as np
 pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
-    """Function that calculates the expectation step in the EM algorithm for a
+    """Function that calculates the expectation step in the EM algorithm For a
     GMM:
         X is a numpy.ndarray of shape (n, d) containing the data set
-        pi is a numpy.ndarray of shape (k,) containing the priors for each
+        pi is a numpy.ndarray of shape (k,) containing the priors For each
         cluster
-        m is a numpy.ndarray of shape (k, d) containing the centroid means for
+        m is a numpy.ndarray of shape (k, d) containing the centroid means For
         each cluster
         S is a numpy.ndarray of shape (k, d, d) containing the covariance
-        matrices for each cluster
+        matrices For each cluster
         Returns: g, l, or None, None on failure
             g is a numpy.ndarray of shape (k, n) containing the posterior
-            probabilities for each data point in each cluster
+            probabilities For each data point in each cluster
             l is the total log likelihood"
     
     """
@@ -30,11 +30,11 @@ def expectation(X, pi, m, S):
         # Initialization of array to store posterior probabilities
         g = np.zeros((k, n))
 
-        # Calculate PDF for each cluster
+        # Calculate PDF For each cluster
         for i in range(k):
             g[i] = pi[i] * pdf(X, m[i], S[i])
 
-        # Calculate total probability for normalization
+        # Calculate total probability For normalization
         total_prob = np.sum(g, axis=0, keepdims=True)
 
         # No division by zero here
