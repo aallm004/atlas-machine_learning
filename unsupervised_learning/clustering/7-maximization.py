1a0839706c2fa25edc218630af74bb2602f512af
aallm004 <abigailrosea10@gmail.com>
2025-01-22 19:40:09 -0600

    new iffy

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -16,10 +16,9 @@ def maximization(X, g, verbose=False):
         means for each cluster
         S is a numpy.ndarray of shape (k, d, d) containing the updated
         covariance matrices for each cluster"""
-    if verbose:
+    if not isinstance(g, np.ndarray):
         print(f'g: {g}')
-    if (not isinstance(X, np.ndarray) or len(X.shape) != 2 or
-            not isinstance(g, np.ndarray) or len(g.shape) != 2):
+    if not np.allclose(g.sum(axis=0), np.ones(n)):
         return None, None, None
     
     try:
