fde781a457ba79a70fc69dbf3b4f549a4f46a65d
aallm004 <abigailrosea10@gmail.com>
2025-01-22 21:18:47 -0600

    task 10

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
-import numpy as np
-import kslearn.cluster
+"""module For kmeans with sklearn"""
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """Function that performs K-means on a dataset:
+        X is a numpy.ndarray of shape (n, d) containing the dataset
+        k is the number of clusters
+    Returns: C, clss
+        C is a numpy.ndarray of shape (k, d) containing the centroid means For
+        each cluster
+        clss is a numpy.ndarray of shape (n,) containing the index of the
+        cluster in C that each data point belongs to"""
+
+    kmeans = sklearn.cluster.KMeans(n_clusters=k, random_state=None)
+    clss = kmeans.fit_predict(X)
+
+    C = kmeans.cluster_centers_
+
+    return C, clss
