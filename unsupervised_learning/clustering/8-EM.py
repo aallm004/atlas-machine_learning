9027ccc9250727854c12ccec6eb602cef29d809b
aallm004 <abigailrosea10@gmail.com>
2025-01-22 21:32:39 -0600

    pycodestyle for 8

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -5,23 +5,36 @@ initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
 maximization = __import__('7-maximization').maximization
 
-def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False, extended=False):
-    """Write a function def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False): that performs the expectation maximization for a GMM:
+
+def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False,
+                             extended=False):
+    """Write a function def expectation_maximization(X, k, iterations=1000,
+    tol=1e-5, verbose=False): that performs the expectation maximization for a
+    GMM:
 
         X is a numpy.ndarray of shape (n, d) containing the data set
         k is a positive integer containing the number of clusters
-        iterations is a positive integer containing the maximum number of iterations for the algorithm
-        tol is a non-negative float containing tolerance of the log likelihood, used to determine early stopping i.e. if the difference is less than or equal to tol you should stop the algorithm
-        verbose is a boolean that determines if you should print information about the algorithm
-    If True, print Log Likelihood after {i} iterations: {l} every 10 iterations and after the last iteration
+        iterations is a positive integer containing the maximum number of
+        iterations for the algorithm
+        tol is a non-negative float containing tolerance of the log likelihood,
+        used to determine early stopping i.e. if the difference is less than or
+        equal to tol you should stop the algorithm
+        verbose is a boolean that determines if you should print information
+        about the algorithm
+    If True, print Log Likelihood after {i} iterations: {l} every 10 iterations
+    and after the last iteration
         {i} is the number of iterations of the EM algorithm
         {l} is the log likelihood, rounded to 5 decimal places
-        
+
         Returns: pi, m, S, g, l, or None, None, None, None, None on failure
-            pi is a numpy.ndarray of shape (k,) containing the priors for each cluster
-            m is a numpy.ndarray of shape (k, d) containing the centroid means for each cluster
-            S is a numpy.ndarray of shape (k, d, d) containing the covariance matrices for each cluster
-            g is a numpy.ndarray of shape (k, n) containing the probabilities for each data point in each cluster
+            pi is a numpy.ndarray of shape (k,) containing the priors for each
+            cluster
+            m is a numpy.ndarray of shape (k, d) containing the centroid means
+            for each cluster
+            S is a numpy.ndarray of shape (k, d, d) containing the covariance
+            matrices for each cluster
+            g is a numpy.ndarray of shape (k, n) containing the probabilities
+            for each data point in each cluster
             l is the log likelihood of the model
         """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
@@ -44,7 +57,7 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False, ext
         if extended:
             print('**e**')
         return None, None, None, None, None
-    
+
     try:
         pi, m, S = initialize(X, k)
         if pi is None or m is None or S is None:
@@ -60,7 +73,8 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False, ext
             return None, None, None, None, None
 
         if verbose:
-            print('Log Likelihood after {} iterations: {:.5f}'.format(0, prev_l))
+            print('Log Likelihood after {} iterations: {:.5f}'.format(0,
+                                                                      prev_l))
 
         for i in range(1, iterations + 1):
 
@@ -78,16 +92,19 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False, ext
 
             if np.abs(current_l - prev_l) <= tol:
                 if verbose:
-                    print('Log Likelihood after {} iterations: {:.5f}'.format(i, current_l))
+                    print('Log Likelihood after' +
+                          ' {} iterations: {:.5f}'.format(i, current_l))
                 return pi, m, S, g, current_l
 
             if verbose and i % 10 == 0:
-                print('Log Likelihood after {} iterations: {:.5f}'.format(i, current_l))
+                print('Log Likelihood after' +
+                      ' {} iterations: {:.5f}'.format(i, current_l))
 
             prev_l = current_l
 
         if verbose and not i % 10 == 0:
-            print('Log Likelihood after {} iterations: {:.5f}'.format(i, prev_l))
+            print('Log Likelihood after {} iterations: {:.5f}'.format(i,
+                                                                      prev_l))
 
         return pi, m, S, g, prev_l
 
