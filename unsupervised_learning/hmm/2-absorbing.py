17e9010ea5c2be22bd4ca0f4669e8daecca77ef1
aallm004 <abigailrosea10@gmail.com>
2025-02-04 16:01:20 -0600

    task 2

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -10,3 +10,32 @@ def absorbing(P):
             P[i, j] is the probability of transitioning from state i to state j
             n is the number of states in the markov chain
             Returns: True if it is absorbing for False on failure"""
+    try:
+        n = P.shape[0]
+        # Find absorbing states
+        absorbing_states = np.where(np.diag(P) == 1)[0]
+
+        # Find absorbing states
+        if len(absorbing_states) == 0:
+            return False
+
+        # Check that rows of absorbing states are correct
+        # (probability of 1 for self-transition, 0 for others)
+        for state in absorbing_states:
+            if not np.allclose(P[state], np.eye(n)[state]):
+                return False
+
+        # Remove absorbing states to get second matrix of non-absorbing
+        non_absorbing = np.setdiff1d(np.arange(n), absorbing_states)
+        if len(non_absorbing) == 0:
+            return True
+
+        # Get submatrix of non-absorbing states transitioning to each other
+        submatrix = P[non_absorbing][:, non_absorbing]
+
+        # Check if 1 is not an eigenvalue of submatrix
+        eigenvals = np.linalg.eigvals(submatrix)
+        return not np.any(np.isclose(eigenvals, 1))
+
+    except (np.linalg.LinAlgError, ValueError, IndexError):
+        return False
