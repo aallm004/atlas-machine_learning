abca61d39cfb2327ccc1455c607704a8dcbde1f6
aallm004 <abigailrosea10@gmail.com>
2025-02-05 15:19:14 -0600

    task 3

--- unsupervised_learning/hmm/3-forward.py
+++ unsupervised_learning/hmm/3-forward.py
@@ -26,5 +26,47 @@ def forward(Observation, Emission, Transition, Initial):
             probabilities
                 F[i, j] is the probability of being in hidden state i at time j
                 given the previous observations"""
-    
+    try:
+        obvs = Observation.shape[0]
+        hidden_states, possible = Emission.shape
 
+        # Shape Check
+        if Transition.shape != (hidden_states, hidden_states):
+            return None, None
+
+        if Initial.shape != (hidden_states, 1):
+            return None, None
+
+        if not np.all(Observation < possible):
+            return None, None
+
+        # Probability constraints
+        if not (np.allclose(np.sum(Emission, axis=1), 1) and
+                np.allclose(np.sum(Transition, axis=1), 1) and
+                np.allclose(np.sum(Initial), 1)):
+            return None, None
+
+        # Are there negative probabilities?
+        if np.any(Emission < 0) or np.any(Transition < 0) or \
+           np.any(Initial < 0):
+            return None, None
+
+    except (AttributeError, IndexError, ValueError):
+        return None, None
+
+    # Initialization of forward path probability matrix
+    forward_path = np.zeros((hidden_states, obvs))
+
+    # Initialization of first timestep
+    forward_path[:, 0] = Initial.flatten() * Emission[:, Observation[0]]
+
+    # Forward algorithm recursion
+    for i in range(1, obvs):
+        for state in range(hidden_states):
+            forward_path[state, i] = np.sum(forward_path[:, i-1] *
+                                            Transition[:, state]) * \
+                                            Emission[state, Observation[i]]
+
+    likelihood = np.sum(forward_path[:, -1])
+
+    return likelihood, forward_path
