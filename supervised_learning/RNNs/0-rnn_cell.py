d2c2313dd72e93e2322fb6e4ac4ef8a10e95bec7
aallm004 <abigailrosea10@gmail.com>
2025-03-10 16:53:07 -0500

    pycodestyle

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -17,18 +17,18 @@ class RNNCell:
         # Bias for output
         self.by = np.zeros((1, o))
 
-
-
     def forward(self, h_prev, x_t):
         """performs forward propagation for one time step
-        x_t is a numpy.ndarray of shape (m, i) that contains the data input for the cell
+        x_t is a numpy.ndarray of shape (m, i) that contains the data input for
+        the cell
             m is the batche size for the data
-            h_prev is a numpy.ndarray of shape (m, h) containing the previous hidden state
+            h_prev is a numpy.ndarray of shape (m, h) containing the previous
+            hidden state
         The output of the cell should use a softmax activation function
         Returns: h_next, y
         h_next is the next hidden state
         y is the output of the cell"""
-        
+
         # Concatenate previous hidden state and current input data
         concat_input = np.concatenate((h_prev, x_t), axis=1)
 
