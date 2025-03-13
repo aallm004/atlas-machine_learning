97c886fa8e179414f4f61a8d0d4bf7c051ab4104
aallm004 <abigailrosea10@gmail.com>
2025-03-13 10:40:51 -0500

    missing ()

--- supervised_learning/RNNs/5-bi_forward.py
+++ supervised_learning/RNNs/5-bi_forward.py
@@ -30,20 +30,20 @@ class BidirectionalCell:
         self.by = np.zeros((1, o))
 
 
-        def forward(self, h_prev, x_t):
-            """calculates the hidden state in forward direction for one time
-            step
-                x_t is a numpy.ndarray of shape (m, i) that contains the data
-                input for the cell
-                    m is the batch size for the data
-                    h_prev is a numpy.ndarray of shape (m, h) containing the
-                    previous hidden state
-            Returns: h_next, the next hidden state
-            """
-            # concatenates h_prev and x_t
-            concat = np.concatenate(h_prev, x_t, axis=1)
+    def forward(self, h_prev, x_t):
+        """calculates the hidden state in forward direction for one time
+        step
+            x_t is a numpy.ndarray of shape (m, i) that contains the data
+            input for the cell
+                m is the batch size for the data
+                h_prev is a numpy.ndarray of shape (m, h) containing the
+                previous hidden state
+        Returns: h_next, the next hidden state
+        """
+        # concatenates h_prev and x_t
+        concat = np.concatenate((h_prev, x_t), axis=1)
 
-            # Calculates next hidden state using tahn
-            h_next = np.tahn(np.dot(concat, self.Whf) + self.bhf)
+        # Calculates next hidden state using tahn
+        h_next = np.tahn(np.dot(concat, self.Whf) + self.bhf)
 
-            return h_next
+        return h_next
