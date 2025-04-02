8bb748397faa3a27d089ffe8137d430b5a417a6f
aallm004 <abigailrosea10@gmail.com>
2025-04-02 18:00:50 -0500

    maybe will be worth it points wise, if only to get some?

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -6,9 +6,23 @@ import tensorflow as tf
 def gensim_to_keras(model):
     """model is a trained gensim word2vec models
     Returns: the trainable keras Embedding"""
-
+    # .wv can have:
+    # vectors: numpy array of shape (vacab size and vector size)
+        # with the word embeddings
+    # key_to_index: dict that maps words(as strings) to their correct indices
+        # in vector array
+    # index_to_key: list that maps indices back to words, so you can see which
+        # word corresponds to which vector
+    
     # Get vocab size and vector dimensions from model
+    
+    # NumPy array that contains all word vectors
+    
+    ## Each row goes to a word in the vocab
+    ## Each column is a dimension of the embedding space
     vocab = model.wv.vectors.shape[0]
+    
+    ## Shows how many numbers are used to represent each word
     vector = model.wv.vectors.shape[1]
 
     # Extract word vectors from model
---
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""module for fastext"""
+import tensorflow as tf
+from gensim.models import FastText
+
+def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
+                   window=5, cbow=True, epochs=5, seed=0, workers=1):
+    """Function that creates, builds and trains a genism fastText model
+        sentences: list of sentences to be trained on
+        vector_size: dimensionality of the embedding layer
+        min_count: min number of occurrences of a word for use in training
+        window: max distance between current and predicted word within sentence
+        negative: size of negative sampling
+        cbow: a boolean to determine the training type;
+            True is for CBOW
+            False is for Skip-gram
+        epoch: number of iterations to train over
+        seed: seed for the random number generator
+        workers: number of worker threads to train the model
+
+    Return: the trained model"""
+    # Set training algorithm for cbow
+    # Because it's CBOW sg = 0, otherwise it would be sg = 1
+    sg = 0 if cbow else 1
+
+    # fastText model with specific parameters
+    model = FastText(
+        vector_size=vector_size,
+        window=window,
+        min_count=min_count,
+        negative=negative,
+        sg=sg,
+        seed=seed,
+        workers=workers,
+        epochs=epochs
+    )
+
+    # Build the vocab from sentences
+    model.build_vocab(sentences)
+    
+    # Train model on the input sentences
+    model.train(
+        sentences,
+        total_examples=model.corpus_count,
+        epochs=model.epochs
+    )
+
+    return model
