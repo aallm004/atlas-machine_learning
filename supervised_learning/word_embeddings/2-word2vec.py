fcc21d8e4ac26d636717a28988a48bded46d40d7
aallm004 <abigailrosea10@gmail.com>
2025-04-03 09:02:51 -0500

    pycodestyle

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -25,14 +25,14 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     sg = 0 if cbow else 1
 
     model = gensim.models.Word2Vec(sentences=sentences,
-                     vector_size=vector_size,
-                     min_count=min_count,
-                     window=window,
-                     negative=negative,
-                     sg=sg,
-                     epochs=epochs,
-                     seed=seed,
-                     workers=workers)
+                                   vector_size=vector_size,
+                                   min_count=min_count,
+                                   window=window,
+                                   negative=negative,
+                                   sg=sg,
+                                   epochs=epochs,
+                                   seed=seed,
+                                   workers=workers)
 
     # Use build_vocab (builds vocab from sentences) by:
     # Counting word frequencies
