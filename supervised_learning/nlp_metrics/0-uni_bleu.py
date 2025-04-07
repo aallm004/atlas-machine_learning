90f6f33bc96385189bb28ec0b7c1b4653157f779
aallm004 <abigailrosea10@gmail.com>
2025-04-07 16:02:31 -0500

    brevity penalty

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -12,6 +12,11 @@ def uni_bleu(references, sentence):
     Returns: the unigram BLEU score"""
     length_of_sentence = len(sentence)
 
+    # Find the reference with the closest length to the candidate
+    closest_ref_idx = np.argmin([abs(len(reference) - length_of_sentence)
+                                for reference in references])
+    closest_ref_length = len(references[closest_ref_idx])
+
     # Count each word that appears in sentence
     count = {}
     for word in sentence:
@@ -19,12 +24,18 @@ def uni_bleu(references, sentence):
             max_count = 0
             for reference in references:
                 max_count = max(max_count, reference.count(word))
-            count[word] = max_count                
+            count[word] = max_count
 
     # Calculate clipped count
     count_clipped = sum(min(sentence.count(word), count)
                         for word, count in count.items())
 
-    bleu_score = count_clipped / length_of_sentence
+    # Calculate precision
+    precision = count_clipped / length_of_sentence
+
+    # Brevity penalty
+    bp = np.exp(1 - closest_ref_length / length_of_sentence)
+
+    bleu_score = bp * precision
 
     return bleu_score
