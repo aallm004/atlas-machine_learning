bc3ebab3b49a51e9d3ff3138fa629da8c0de60a2
aallm004 <abigailrosea10@gmail.com>
2025-04-09 16:45:52 -0500

    epochsepochsepochs

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -39,7 +39,7 @@ def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
     model.train(
         sentences,
         total_examples=model.corpus_count,
-        epochs=model.epochs
+        epochs=epochs
     )
 
     return model
