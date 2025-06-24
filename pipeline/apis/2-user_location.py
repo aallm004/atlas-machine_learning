7b229a8bf88a5e8419b573db925956bcccf5e923
aallm004 <abigailrosea10@gmail.com>
2025-06-24 16:01:35 -0500

    no try

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -7,32 +7,31 @@ from datetime import datetime
 
 def get_user_location(api_url):
     """Script that prints the location of a specific user"""
-    try:
-        response = requests.get(api_url)
 
-        # Handle rate limit (403)
-        if response.status_code == 403:
-            reset_time = int(response.headers.get('X-Ratelimit-Reset', 0))
-            current_time = int(datetime.now().timestamp())
-            minutes_left =  max(0, (reset_time - current_time) // 60)
-            print(f"Reset in {minutes_left} min")
+    response = requests.get(api_url)
+
+    # Handle rate limit (403)
+    if response.status_code == 403:
+        reset_time = int(response.headers.get('X-Ratelimit-Reset', 0))
+        current_time = int(datetime.now().timestamp())
+        minutes_left =  max(0, (reset_time - current_time) // 60)
+        print(f"Reset in {minutes_left} min")
+        return
 
 
-        # User not found (404)
-        if response.status_code == 404:
-            print("Not found")
-            return
-        
-        # Handle successful response (200)
-        if response.status_code == 200:
-            user_data = response.json()
-            location = user_data.get('location')
-
-            if location:
-                print(location)
-            else:
-                print ("Not found")
-        else:
-            print("Not Found")
-    except requests.RequestException:
+    # User not found (404)
+    if response.status_code == 404:
         print("Not found")
+        return
+    
+    # Handle successful response (200)
+    if response.status_code == 200:
+        user_data = response.json()
+        location = user_data.get('location')
+
+        if location:
+            print(location)
+        else:
+            print ("Not found")
+    else:
+        print("Not Found")
