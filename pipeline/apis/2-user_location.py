f414d63ef07fef39d69c292ca9b4317b5eed8bf1
aallm004 <abigailrosea10@gmail.com>
2025-06-24 16:14:23 -0500

    pycodestyle

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -14,7 +14,7 @@ def get_user_location(api_url):
         if response.status_code == 403:
             reset_time = int(response.headers.get('X-Ratelimit-Reset', 0))
             current_time = int(datetime.now().timestamp())
-            minutes_left =  max(0, (reset_time - current_time) // 60)
+            minutes_left = max(0, (reset_time - current_time) // 60)
             print(f"Reset in {minutes_left} min")
             return
 
@@ -22,7 +22,7 @@ def get_user_location(api_url):
         if response.status_code == 404:
             print("Not found")
             return
-        
+
         # Handle successful response (200)
         if response.status_code == 200:
             user_data = response.json()
@@ -31,16 +31,17 @@ def get_user_location(api_url):
             if location:
                 print(location)
             else:
-                print ("Not found")
+                print("Not found")
         else:
             print("Not Found")
     except requests.RequestException:
         print("Not found")
 
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: ./2-user_location.py <github_api_url>")
         sys.exit(1)
-    
+
     api_url = sys.argv[1]
     get_user_location(api_url)
