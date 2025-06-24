76c9ef9c84e83dc77da54c5a00a5425a1e651af2
aallm004 <abigailrosea10@gmail.com>
2025-06-24 16:33:15 -0500

    pass

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -44,6 +44,7 @@ def get_rocket_launch_frequency():
     except requests.RequestException:
         pass
     except Exception:
+        pass
         
 if __name__ == '__main__':
     get_rocket_launch_frequency()
