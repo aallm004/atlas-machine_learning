1f2d6c957fe19ebb788609af444f7be926d7fbd8
aallm004 <abigailrosea10@gmail.com>
2025-07-02 15:15:55 -0500

    ;

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -9,12 +9,13 @@ BEGIN
     DECLARE avg_score DECIMAL(5,2);
     
     -- Calculate the average score
-    SELECT AVG(score) INTO avg_score
+    SELECT AVG(score)
+    INTO avg_score
     FROM corrections
     WHERE corrections.user_id = user_id;
     
     -- Update the user's average_score
     UPDATE users SET average_score = avg_score
     WHERE id = user_id;
-END$$
+END;
 //
